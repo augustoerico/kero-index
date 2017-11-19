805a49733985308f94bfdc128d8537f372e0c624
Erico Silva <augusto.ericosilva@gmail.com>
2017-11-19 20:33:57 -0200

    Add product extraction to JSON

--- store_search_bar/store_search_bar/spiders/store_spider.py
+++ store_search_bar/store_search_bar/spiders/store_spider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+from functools import partial
 
 
 class StoreSpider(scrapy.Spider):
@@ -11,8 +12,18 @@ class StoreSpider(scrapy.Spider):
     @staticmethod
     def parse_product(response):
         body = response.body.decode('utf-8')
-        matcher = re.search('"appDefinitionName":"Wix Stores","instance":"(.+?)"', body)
+        matcher = re.search('<script\s+type=\"application/ld\+json\">(.+)</script>', body)
+        product_details = matcher.group(1)
+        yield {
+            'product': product_details
+        }
 
+    @staticmethod
+    def parse_product_page(product, response):
+        body = response.body.decode('utf-8')
+        matcher = re.search('"appDefinitionName":"Wix Stores","instance":"(.+?)"', body)
+        url = 'https://ecom.wix.com/storefront/product/' + product + '?instance=' + matcher.group(1)
+        yield scrapy.Request(url=url, callback=StoreSpider.parse_product)
 
     @staticmethod
     def parse_gallery(response):
@@ -20,11 +31,14 @@ class StoreSpider(scrapy.Spider):
         matches = re.findall('"urlPart":"(.+?)"', body)
 
         for m in matches:
-            url = "https://www.queromania.com/product-page/" + m
-            yield scrapy.Request(url=url, callback=StoreSpider.parse_product)
+            url = 'https://www.queromania.com/product-page/' + m
+            callback = partial(StoreSpider.parse_product_page, m)
+            yield scrapy.Request(url=url, callback=callback)
 
     def parse(self, response):
         body = response.body.decode('utf-8')
         matcher = re.search('"appDefinitionName":"Wix Stores","instance":"(.+?)"', body)
-        url = "https://ecom.wix.com/storefront/gallery?instance=" + matcher.group(1)
+        url = 'https://ecom.wix.com/storefront/gallery?instance=' + matcher.group(1)
         yield scrapy.Request(url=url, callback=StoreSpider.parse_gallery)
+
+        # TODO yield "See more"
